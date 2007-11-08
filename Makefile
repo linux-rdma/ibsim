1bbc958866572437bb5e04382757247a859c6e16
Sasha Khapyorsky <sashak@voltaire.com>
2007-11-08 16:47:53 +0200

    dist.sh: ibsim dist script
    ibsim dist script. Also 'make dist' target for Makefile.
    
    Signed-off-by: Sasha Khapyorsky <sashak@voltaire.com>

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ subdirs:= ibsim umad2sim
 
 all clean dep install:
 	$(foreach dir, $(subdirs), make -C $(dir) $@ && ) echo "Done."
+
+dist:
+	./dist.sh
---
+++ dist.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -xv
+
+ver=`cat ibsim/ibsim.c | sed -ne '/#define IBSIM_VERSION /s/^#define IBSIM_VERSION \"\(.*\)\"/\1/p'`
+rel=1
+distdir=ibsim-${ver}
+tarball=${distdir}.tar.gz
+
+rm -f $tarball
+rm -rf $distdir
+mkdir $distdir
+
+files=`find . -name '*.[ch]' -o -name Makefile`
+cp -a --parents $files \
+	   defs.mk README COPYING TODO net-examples scripts $distdir
+
+cat ibsim.spec.in \
+		| sed -e 's/@VERSION@/'$ver'/' -e 's/@RELEASE@/'$rel'/' -e 's/@TARBALL@/'$tarball'/' \
+		> $distdir/ibsim.spec
+
+tar czf $tarball $distdir
+rm -rf $distdir
